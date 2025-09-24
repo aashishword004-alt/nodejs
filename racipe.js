5e6ebc92978f515874ad036f12ae722e3a847bcb
aashishword004-alt <aashishword004@gmail.com>
2025-09-24 15:02:17 +0530

    requestsend

--- racipe.js
+++ racipe.js
@@ -8,19 +8,22 @@ var server = http.createServer(function (request, response) {
     //code in this function will run for each and every requested
     let url = request.url;
     console.log(url);
-    response.writeHead(200,{'content-type':'text/html'});
+    response.writeHead(200, { 'content-type': 'text/html' });
     if (url === '/')//home
     {
-        response.write("<html><head></head><body><h1>Home</h1><p>this is home page</p></body></html>")
+        response.write("<html><head><title>Recipe List</title></head><body><h1>Home</h1><p>This is home page</p><p>Pav Bhaji, Dhokla, Pani Puri, Pulav</p></body></html>");
     }
-    else if (url === '/aboutus') {
-        response.write("<html><head></head><body><h1>about us</h1><p>this is about us page</p></body></html>")
+    else if (url === '/pavbhaji') {
+        response.write("<html><head><title>Recipe List</title></head><body><h1>Home</h1><p>This is home page</p><p>Pav Bhaji: Mix vegetables and cook with pav bhaji masala, Dhokla: Steam fermented batter of rice and chickpeas, Pani Puri: Fill puris with spiced water and potato, Pulav: Cook rice with vegetables and spices</p></body></html>");
     }
-    else if (url === '/contactus') {
-        response.write("<html><head></head><body><h1>contact us</h1><p>this is contact us page</p></body></html>")
+    else if (url === '/dhokla') {
+        response.write("<html><head><title>Dhokla</title></head><body><h1>Dhokla</h1><p>How to make Dhokla: Steam fermented batter of rice and chickpeas.</p></body></html>");
     }
-    else if (url === '/products') {
-        response.write("<html><head></head><body><h1>products</h1><p>this is products page</p></body></html>")
+    else if (url === '/panipuri') {
+        response.write("<html><head><title>Pani Puri</title></head><body><h1>Pani Puri</h1><p>How to make Pani Puri: Fill puris with spiced water and potato.</p></body></html>");
+    }
+    else if (url === '/pulav') {
+        response.write("<html><head><title>Pulav</title></head><body><h1>Pulav</h1><p>How to make Pulav: Cook rice with vegetables and spices.</p></body></html>");
     }
     response.end();
 });
