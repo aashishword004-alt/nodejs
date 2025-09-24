72723de8a0f70c65f9747935944548f6c3d7f8ca
aashishword004-alt <aashishword004@gmail.com>
2025-09-24 11:49:17 +0530

    pandding

--- racipe.js
+++ racipe.js
@@ -1,20 +1,29 @@
 var http = require('http');
-// home racipe
-// panipur
-//pavbaji
-//pulav
- var server = http.createServer(function (request, response) {
- let url = request.url;
- console.log(url);
- response.writeHead(200,{'content-type':'text/html'})
- if(url === '/') /// home
- {
-    response.writeHead("<html><head><meta> <title>Recipe List</title></head><body><h1>Home</h1><h2>Recipes</h2><ul><li>Pav Bhaji</li><li>Dhokla</li><li>Pani Puri</li><li>Pulav</li></ul></body></html>")
- }
- else{
-
- }
- response.end();
-})
+//localhost:3000 (home)
+//localhost:3000/aboutus (aboutus)
+//localhost:3000/contactus (contactus)
+//localhost:3000/products (products)
+//create server 
+var server = http.createServer(function (request, response) {
+    //code in this function will run for each and every requested
+    let url = request.url;
+    console.log(url);
+    response.writeHead(200,{'content-type':'text/html'});
+    if (url === '/')//home
+    {
+        response.write("<html><head></head><body><h1>Home</h1><p>this is home page</p></body></html>")
+    }
+    else if (url === '/aboutus') {
+        response.write("<html><head></head><body><h1>about us</h1><p>this is about us page</p></body></html>")
+    }
+    else if (url === '/contactus') {
+        response.write("<html><head></head><body><h1>contact us</h1><p>this is contact us page</p></body></html>")
+    }
+    else if (url === '/products') {
+        response.write("<html><head></head><body><h1>products</h1><p>this is products page</p></body></html>")
+    }
+    response.end();
+});
+//let us start server
 server.listen(5000);
-console.log('server startted')
+console.log('server started...');
