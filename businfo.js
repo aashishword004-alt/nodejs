eb2012516b52ea5074faf1250eab74d5ed09a386
aashishword004-alt <aashishword004@gmail.com>
2025-10-07 19:09:56 +0530

    busis done

--- businfo.js
+++ businfo.js
@@ -1,19 +1,33 @@
+// server.js
 let http = require('http');
+let url = require('url');
+let businf = require('./module/bus'); 
 
-// let  url = require('url');
+let server = http.createServer(function (req, res) {
+    res.writeHead(200, { 'Content-Type': 'application/json' });
 
-let businf = require('./module/bus');
+    let busobject = url.parse(req.url,true);
+    let qurystring = busobject.query;
 
-let  server = http.createServer( function (req,res){
- 
-    res.writeHead(200,{'content-type':'application/json'});
-    // let qury = url.parse(res.url,true);
-    // let filter = businf;
     let output = businf;
-    
-    res.write(JSON.stringify(output));
-    res.end();
-})
 
-server.listen(5000);
-console.log("Serever is start");
+
+    output = output.filter(bus => {
+        if (qurystring.source && bus.from !== qurystring.source) return false;
+
+        if (qurystring.destination && bus.to !== qurystring.destination) return false;
+
+        if (qurystring.acType && bus.acType !== qurystring.acType) return false;
+
+        if (qurystring.seatType && bus.seatType !== qurystring.seatType) return false;
+
+        if (qurystring.rent && bus.rent != Number(qurystring.rent)) return false;
+
+        return true; //
+    });
+
+    res.write(JSON.stringify(output, null, 2));
+    res.end();
+});
+
+server.listen(5000, () => console.log("Server is running on port 5000"));
