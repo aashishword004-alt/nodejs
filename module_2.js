04810d6c4f881d2e08752c37cc732c8225c32da7
aashishword004-alt <aashishword004@gmail.com>
2025-10-10 16:55:54 +0530

    404pandd

--- module_2.js
+++ module_2.js
@@ -3,10 +3,44 @@
 
 let http = require("http");
 
+const fs= require("fs");
+
+
 let server = http.createServer((req,res) =>{
-    res.writeHead(200,{"Content-type":"text/plain"});
-    res.write("Serever is lisning");
+
+    let url = req.url;
+
+    let filename = "site/404.html"
+
+    if(url == "/" || url == "/home")
+    {
+         filename = "site/home.html" 
+    }
+    else if(url == "/aboutus"){
+        
+        filename="site/about.html"
+
+    }
+    else if(url == "/contact"){
+    
+        filename="site/contact.html"
+
+    }
+     fs.readFile(filename , (error  , data) =>{
+     res.writeHead(200,{"Content-type":"text/html"});
+     if(error)
+     {
+        filename="/site/404.html"
+
+     }
+     else{
+
+         res.write(data);
+     }
     res.end();
+
+   })    
+   
 });
 
 let port = 5000;
