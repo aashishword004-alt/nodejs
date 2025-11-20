d5740e386d9b5ae922af17aa40bdaafd39bb1648
aashishword004-alt <aashishword004@gmail.com>
2025-11-20 22:31:51 +0530

    done

--- database/currency.js
+++ database/currency.js
@@ -5,6 +5,12 @@ let curr = require("../lib/ruppesconvert");
 let server = http.createServer( (req,res) =>{
 
     res.writeHead(200,{"Content-Type" :"text/plain"});
+    let dol = curr.doll(8400);
+    let pd = curr.pd(10200);
+    let ero = curr.ero(9000);
+    res.write("Dollar :" + dol + "\n");
+    res.write("Pound :" + pd + "\n");
+    res.write("Euro :" + ero + "\n");
     res.write("Welcome to Exchage Rate server ");
     res.end();
 
