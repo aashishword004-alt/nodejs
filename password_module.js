92d8c5a23cbfd0dbdfc4ebc7065d3a137fb6fc2d
aashishword004-alt <aashishword004@gmail.com>
2025-11-22 17:50:55 +0530

    conformpassword pandding

--- password_module.js
+++ password_module.js
@@ -1,21 +1,26 @@
 var http = require("http");
 let sequrity = require("./lib/securityy");
 
-function hadlePassdword (req,res){
-    res.writeHead(200,{"Content_Type": "text/plain"});
-   sequrity. getpassword("xyx").then() {
-    res.write("hash password is : "  + hash);   
-   } 
+async function hadlePassdword(req, res) {
+    let password = await sequrity.generatePassword();
+    sequrity.getpassword(password).then((hash) => {
+        res.write(password);
+        res.write("\n");
+        res.write(hash);
+        console.log(password);
+        console.log("////");
+        console.log(hash);
+    });
+
+
+    res.writeHead(200, { "Content-type": "text/plain" });
     res.end();
 
+}
 
-} 
-
-
-let server = http.createServer((req,res) => hadlePassdword(req,res){
-});
+let server = http.createServer((req, res) => hadlePassdword(req, res));
 
 let port = 3000;
-server.listen(port , () =>{
-    console.log("Server is runnig on port " + port) 
+server.listen(port, () => {
+    console.log("Server is runnig on port " + port)
 })
