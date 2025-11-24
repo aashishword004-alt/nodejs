ca2f1ba67d9d93623b9030d5d7187dec5e323501
aashishword004-alt <aashishword004@gmail.com>
2025-11-24 11:39:57 +0530

    in function

--- database/otpp.js
+++ database/otpp.js
@@ -2,13 +2,19 @@
 let http = require("http");
 let password = require("../lib/otp");
 
-
-let server = http.createServer((req,res) =>{
+ function eamil (req,res) {
   res.writeHead(200,{"Content-Type" : "text/plain"})
   let output = password()
   console.log(output);
   res.write(output);
   res.end()
+
+ }
+
+
+let server = http.createServer((req,res) =>{
+  eamil(req,res)
+  
 
 });
 let port = 5000;
