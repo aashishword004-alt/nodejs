308383421fa7af47b922911ceea3d47c88cf0baf
aashishword004-alt <aashishword004@gmail.com>
2025-11-27 15:13:20 +0530

    ex done

--- fs_write.js
+++
@@ -1,9 +0,0 @@
-let http = require('http');
-let url = require("url");
-let fs = require("fs");
-
-let server = http.createServer((req,res) =>{
-res.writeHead(200 {"content:type" : "text/html"});
-
-
-})
