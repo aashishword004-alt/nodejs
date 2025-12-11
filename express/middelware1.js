5696268a8a426cf7a52a1f4e1c8b8b39b6ceac9a
aashishword004-alt <aashishword004@gmail.com>
2025-12-11 18:03:48 +0530

    middlware are done

--- express/middelware1.js
+++ express/middelware1.js
@@ -1,6 +1,12 @@
+const { error } = require('console');
 let express = require('express');
 let app = express();
+let fs = require('fs');
+let port = 3000;
 
+let date = new Date();
+let output = date.getHours()  + ":" + date.getMinutes() + "  " +  date.getDate()  + "/" +( date.getMonth() + 1 )+ "/" + date.getFullYear() + "   Port nomber" + port +  "\n";
+let filecontet =  output;
 
 app.use((req, res, next) => {
     console.log("first middleware");
@@ -13,20 +19,35 @@ app.use((req, res, next) => {
 });
 
 app.use((req, res, next) => {
-    let date = new Date();
     date.getHours() 
-    if(date.getHours() > 10 && date.getHours() < 18) res.send("request are acepted");
+    if(date.getHours() > 10 && date.getHours() < 18) console.log("request are acepted");
     else res.send("time out");
     next();
 });
+
+app.use((req,res,next) =>{
+    fs.appendFile("filename.txt", filecontet,(error) =>{
+        if(error)
+        {
+            console.log("something wrong");
+        }
+        else{
+            console.log("file date are saved");
+
+        }
+    });
+    
+    next();
+})
 
 
 let Route = "/";
 
 app.get(Route, (req, res) => {
+    res.send("Server are redy to acept the request");
+    console.log("Redy to acept");
 });
 
-let port = 3000;
 app.listen(port, () => {
     console.log("Serever are stated");
 })
