9dde864cb5743cd899dcae77dcd8f9f9e8f84a07
aashishword004-alt <aashishword004@gmail.com>
2025-12-27 18:05:06 +0530

    no connect

--- database/conneaction.js
+++ database/conneaction.js
@@ -1,5 +1,7 @@
 const mysql = require ('mysql');
 const com = require ('./3_mudule.js');
+const express = require('express');
+const app = express();
 const con = mysql.createConnection({
 
     host:com.server,
@@ -9,12 +11,23 @@ const con = mysql.createConnection({
     port:com.portno
 });
 
+const ROOT = '/api'
+
 con.connect((error) =>{
     if(error)
         console.log(error);
     else
         console.log("Database connnected");
 
+});
+
+app.get(ROOT , (req, res) =>{
+    res.send("API is working");
+    console.log("API is working");
+});
+
+app.listen(3000, () =>{
+    console.log("Server started at port 3000");
 });
 
 module.exports.con = con;
